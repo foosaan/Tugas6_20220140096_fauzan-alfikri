9d05f3a3ba0c4cb19ce1662dc87047956c56d874
fauzan alfikri <112147642+foosaan@users.noreply.github.com>
2024-04-06 16:02:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+audio.addEventListener("click", function () {
+  alert("ini audio");
+});
+
+video.addEventListener("click", function () {
+  alert("ini video");
+});
+
+gambar.addEventListener("click", function () {
+  alert("ini gambar");
+});
